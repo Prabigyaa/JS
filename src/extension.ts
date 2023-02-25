cda06bac1d54ac9b6ee259fd9425368842e56f9c
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-02-25 13:22:31 +0545

    Using different way to find out if python is available.

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import * as path from 'path';
 
 // Import the language client, language client options and server options from VSCode language client.
 import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
+import { once } from 'events';
 
 let client: LanguageClient;
 
@@ -12,21 +13,40 @@ let client: LanguageClient;
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 
+	let foundPython = false;
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "nlp-bridge" is now active!');
 
-	// finding out python
-	const { python3 } = process.env;
-	console.log(`Using python3 : ${python3}`);
+    let spawn = require('child_process').spawn,
+        py    = spawn('python3', ['-c', "print('Hello World')"]);
+
+
+    py.stdout.on('data', () => {
+		console.log("Found python installation.");
+        foundPython = true;
+
+		vscode.window.showInformationMessage("Found python3");
+    });
+
+    // Handle error output
+    py.stderr.on('data', (data: string) => {
+        console.log("Error: ", data);
+		foundPython = false;
+
+		vscode.window.showErrorMessage("Couldn't find python3");
+    });
+
+	await once(py, 'close');
 
 	// if python3 is available continue
-	if (python3) {
+	if (foundPython) {
 		// python3 execution path.
-		let excecutable: string = python3;
+		let excecutable: string = "python3";
 
 		// path to the server.py
-		let serverPath = path.join(__dirname, '..', '..', 'server', 'src', 'server.py');
+		let serverPath = path.join(__dirname, '..', 'server', 'src', 'server.py');
 		console.log('Executing ', serverPath);
 		const args: string[] = [serverPath];
 
