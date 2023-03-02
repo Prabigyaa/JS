970ac2d17e4a1b72e4f5993bc50133c04ae3c189
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-03-02 20:05:59 +0545

    Use comment-parser module to extract comments from documents.

--- server/src/nlpserver/server.py
+++ server/src/nlpserver/server.py
@@ -4,16 +4,23 @@ from lsprotocol.types import (
     CompletionItem,
     CompletionList,
     CompletionParams,
+    TEXT_DOCUMENT_DID_CHANGE,
+    DidChangeTextDocumentParams
 )
 
-from typing import Literal
-
-VARIABLE_NAME_CHANGE: Literal = "variableName/change"
+from comment_parser import comment_parser
 
 server = LanguageServer('nlpserver', 'v0.1')
 
+
 @server.feature(TEXT_DOCUMENT_COMPLETION)
 def completions(params: CompletionParams):
+    '''
+        Just for checking if the extension initialized successfully.
+
+        When `hello.` is typed and `ctrl + space` is pressed,
+        it should give completion options `world` and `friend`.
+    '''
     items = []
     document = server.workspace.get_document(params.text_document.uri)
     current_line = document.lines[params.position.line].strip()
@@ -24,17 +31,39 @@ def completions(params: CompletionParams):
         ]
     return CompletionList(is_incomplete=False, items=items)
 
-def change_variable_name(params: any):
+@server.feature(TEXT_DOCUMENT_DID_CHANGE)
+def add_quick_fix_required(params: DidChangeTextDocumentParams):
     document = server.workspace.get_document(params.text_document.uri)
 
-    # getting the comments
-    comment_list = []
-    line = 1
-    while(line <= document.lines()):
-        if(document.lines[line].strip().startswith('#')):
-            comment_list.append(document.lines[line])
-    
-    server.send_notification(VARIABLE_NAME_CHANGE, comment_list)
-    
+    mime = None
+    language_id = document.language_id
 
-server.start_io()
+    if language_id=="python":
+        mime="text/x-python"
+    elif language_id=="javascript":
+        mime="application/javascript"
+    elif language_id=="c++":
+        mime="text/x-c++"
+    elif language_id=="c":
+        mime="text/x-c"
+    elif language_id=="html":
+        mime="text/html"
+    elif language_id=="java":
+        mime="text/x-java-source"
+    elif language_id=="shell":
+        mime="text/x-shellscript"
+
+    comments = comment_parser.extract_comments(document.path, mime=mime)
+
+    # making a list
+    comments_list = []
+    for comment in comments:
+        comments_list.append([comment.line_number(), comment.text(), comment.is_multiline()])
+
+    server.show_message_log(comments_list.__str__())
+
+def start_server():
+    server.start_io()
+
+if __name__=="__main__":
+    start_server()
