80a4b02a005168e5eae2b50f4cebeb24562be419
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-03-12 15:54:15 +0545

    Some code refactoring and logging build success/failure.

--- server/src/nlpserver/treesitter.py
+++ server/src/nlpserver/treesitter.py
@@ -1,14 +1,12 @@
 import pathlib
 import os
-from subprocess import CalledProcessError
 
 from tree_sitter import Language, Parser
 
 from pathlib import Path
 import sys
 
-from typing import Callable, Any
-from concurrent.futures import Future
+from typing import Callable
 
 import asyncio
 
@@ -19,15 +17,18 @@ src_path = module_path.parents[1]
 if str(src_path) not in sys.path:
     sys.path.append(str(src_path))  # adding src folder to path
 
+# importing only after src is in system path (python paths)
 from utils.includes import SERVER_DIR
 
 
+# the directory in which the language repositories will be downloaded
 LANGUAGE_REPOS_DIR = pathlib.Path.joinpath(SERVER_DIR, "language_repos")
 
 # creating the language repo dir if it doesn't exist
 if not os.path.exists(LANGUAGE_REPOS_DIR):
     os.mkdir(LANGUAGE_REPOS_DIR)
 
+# output folder for .so file created by build_library function
 BUILD_DIR = pathlib.Path.joinpath(SERVER_DIR, "build")
 
 # creating the build dir if it doesn't exist
@@ -36,13 +37,15 @@ if not os.path.exists(BUILD_DIR):
 
 # languages
 LANGUAGES = ["python", "c"]
+
+# updated only after the language library is built
 LANGUAGE_OBJECTS: list[Language] = []
 LANGUAGES_BEING_PARSED: list[str] = []
 
-# the parser
+# the language parser
 PARSER: Parser = Parser()
 
-# base git url
+# base git url containing the language repos
 BASE_URL = "https://github.com/tree-sitter"
 
 
@@ -56,6 +59,11 @@ async def create_language_objects(
     """
 
     def language_to_process(language):
+        """
+        Useful for mapping each language to process in which it is being cloned.
+
+        Returns Coroutines conatining the processes
+        """
         if log_output is not None:
             log_output(
                 f"Cloning {BASE_URL}/tree-sitter-{language} to {LANGUAGE_REPOS_DIR.__str__()}"
@@ -67,6 +75,7 @@ async def create_language_objects(
             cwd=LANGUAGE_REPOS_DIR.__str__(),
         )
 
+    # the processes being executed or done executing
     processes: list[asyncio.subprocess.Process | Exception] = await asyncio.gather(
         *map(language_to_process, LANGUAGES),
         return_exceptions=True,
@@ -75,20 +84,21 @@ async def create_language_objects(
     # keeping track of failed processes
     failed: list[int] = []
     for i, process in enumerate(processes):
-        if process is asyncio.subprocess.Process:
+        if isinstance(process, asyncio.subprocess.Process):
             return_code = await process.wait()
 
             # this isn't full proof (except POSIX)
-            if return_code is not None and return_code > 0:
+            if return_code is not None and return_code > -1:
                 if log_output is not None:
                     log_output(f"Completed cloning language repo for {LANGUAGES[i]}")
             else:
                 failed.append(i)
                 if log_output is not None:
                     log_output(
-                        f"Failed cloning language repo for {LANGUAGES[i]}"
+                        f"Failed cloning language repo for {LANGUAGES[i]}, Error Code: {return_code}"
                     )
 
+    # if it didn't fail then it probably succeeded
     succeded = [language for i, language in enumerate(LANGUAGES) if i not in failed]
 
     # exit if none of the processes succeded
@@ -99,7 +109,7 @@ async def create_language_objects(
 
     library_path = pathlib.Path.joinpath(BUILD_DIR, "languages.so")
     # building the language library
-    Language.build_library(
+    if Language.build_library(
         # Store the library in the `build` directory
         library_path.__str__(),
         # Include one or more languages
@@ -109,7 +119,9 @@ async def create_language_objects(
             ).__str__()
             for language in succeded
         ],
-    )
+    ):
+        if log_output is not None:
+            log_output(f"Built library with languages {succeded}")
 
     for language in succeded:
         LANGUAGE_OBJECTS.append(Language(library_path.__str__(), language))
@@ -127,7 +139,7 @@ def set_parsing_language(language: str) -> bool:
 
     found: bool = False
     for lang in LANGUAGE_OBJECTS:
-        if lang.name == language:
+        if lang.name.lower() == language.lower():
             found = True
 
             PARSER.set_language(lang)
