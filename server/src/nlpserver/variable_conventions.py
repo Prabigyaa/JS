6f874813937360bb8ac4a1f4f399e15f9a554e5b
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-04-13 10:46:23 +0545

    Replace re.match with re.search to search in whole string.

--- server/src/nlpserver/variable_conventions.py
+++ server/src/nlpserver/variable_conventions.py
@@ -1,5 +1,5 @@
 """
-Determining variable convention using Regular Expression
+Determining variable convention using Regular Expressions.
 """
 
 import re
@@ -32,6 +32,7 @@ def get_convention(variable: str) -> list[VariableConventions]:
     """
 
     containing_capital_letters = r"[A-Z]+"
+    containing_capital_letters_only = r"^[A-Z]+$"
     starting_with_capital_letters = r"^[A-Z]"
     containing_small_letters = r"[a-z]+"
     starting_with_small_letters = r"^[a-z]"
@@ -39,26 +40,31 @@ def get_convention(variable: str) -> list[VariableConventions]:
     starting_with_under_scores = r"^_"
 
     contains_capital_letters = (
-        re.match(containing_capital_letters, variable) is not None
-    )
-    starts_with_capital_letters = (
-        re.match(starting_with_capital_letters, variable) is not None
+        re.search(containing_capital_letters, variable) is not None
     )
 
-    contains_small_letters = re.match(containing_small_letters, variable) is not None
-    starts_with_small_letters = re.match(starting_with_small_letters, variable)
+    contains_capital_letters_only = (
+        re.search(containing_capital_letters_only, variable) is not None
+    )
+
+    starts_with_capital_letters = (
+        re.search(starting_with_capital_letters, variable) is not None
+    )
+
+    contains_small_letters = re.search(containing_small_letters, variable) is not None
+    starts_with_small_letters = re.search(starting_with_small_letters, variable)
 
     starts_with_under_scores = (
-        re.match(starting_with_under_scores, variable) is not None
+        re.search(starting_with_under_scores, variable) is not None
     )
 
     # for different conventions like local variables, private variables or others
     while starts_with_under_scores:
         starts_with_under_scores = (
-            re.match(starting_with_under_scores, variable[1:]) is not None
+            re.search(starting_with_under_scores, variable[1:]) is not None
         )
     # At this point, the remaining underscores (if any) shouldn't in the front
-    contains_under_scores = re.match(containing_under_scores, variable) is not None
+    contains_under_scores = re.search(containing_under_scores, variable) is not None
 
     matched_conventions: list[VariableConventions] = []
 
@@ -69,20 +75,25 @@ def get_convention(variable: str) -> list[VariableConventions]:
         else:
             matched_conventions.append(VariableConventions.Snakecase)
 
-    elif starts_with_capital_letters:
+    if starts_with_capital_letters:
 
-        # if capital letters are present in other places
-        if re.match(containing_capital_letters, variable[1:]) is not None:
+        # if capital letters are present in other places and it's not screaming snake case
+        if re.search(containing_capital_letters, variable[1:]) is not None and contains_small_letters:
             matched_conventions.append(VariableConventions.Pascalcase)
 
-    elif starts_with_small_letters:
+    if starts_with_small_letters:
 
         if contains_capital_letters:
             matched_conventions.append(VariableConventions.Camelcase)
 
-    else:
+    if len(matched_conventions) == 0:
+        # some other cases
 
-        matched_conventions.append(VariableConventions.Undefined)
+        # if there are only capital letters (in case of constants)
+        if contains_capital_letters_only:
+            matched_conventions.append(VariableConventions.Screamingsnakecase)
+        else:  # if the list is empty then the convention isn't known
+            matched_conventions.append(VariableConventions.Undefined)
 
     return matched_conventions
 
@@ -96,6 +107,7 @@ if __name__ == "__main__":
     pascal_case_variables = ["StartServer", "LoopEnd", "CreateConnection"]
     snake_case_variables = ["start_server", "loop_end", "create_connection"]
     screaming_snake_case_variables = ["START_SERVER", "LOOP_END", "CREATE_CONNECTION"]
+    mixed_case_variables = ["start_Server", "Loop_End", "STartThis", "CRATECONNECTION"]
 
     print("\n______camelCase______")
     for var in camel_case_variables:
@@ -114,5 +126,10 @@ if __name__ == "__main__":
 
     print("\n______SCREAMING_SNAKE_CASE______")
     for var in screaming_snake_case_variables:
+        output_convention = get_convention(var)
+        print(f"Name: {var}, Convention: {output_convention}")
+
+    print("\n______mixed_case______")
+    for var in mixed_case_variables:
         output_convention = get_convention(var)
         print(f"Name: {var}, Convention: {output_convention}")
